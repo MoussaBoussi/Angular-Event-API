62bf203d6c0dcb3d6709385ee40f7315d66fc963
Matthew Mroue <mamroue@yahoo.com>
2019-02-20 13:07:36 -0500

    results update

--- app/ResultsPage/results.component.js
+++ app/ResultsPage/results.component.js
@@ -8,7 +8,8 @@ const resultsPage = {
     controller: ["Api", function(Api){
         const vm = this;
         vm.eventData = Api.getJSON();                   //event data property equal to Api service and event data getter
-        vm.events = vm.eventData.data._embedded.events;    // event property to the embedded events in console
+        vm.events = vm.eventData.data._embedded.events;
+        // event property to the embedded events in console
         // vm.saveFav = (event) => {
         //     Api.setFav(event);
         // };
